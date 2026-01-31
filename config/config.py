2e5364ce71c0715369ecc0ba6987a6a27d795071
Riya Shinde <shinderiya1912@gmail.com>
2026-01-31 16:31:55 +0530

    config_updated

--- config/config.py
+++ config/config.py
@@ -1,8 +1,7 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-HF_TOKEN = os.getenv("HF_TOKEN")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+HF_TOKEN = st.secrets["HF_TOKEN"]
+
 MODEL_NAME = "groq:qwen/qwen3-32b"
